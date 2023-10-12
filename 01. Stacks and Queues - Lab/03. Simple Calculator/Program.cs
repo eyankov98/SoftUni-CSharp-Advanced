ff15d0d38bb857e7a94aa9e8301f00dff5249578
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-12 17:26:42 +0300

    Add files via upload

---
+++ 01. Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Stack<string> expression = new Stack<string>(input.Reverse());
+
+int result = int.Parse(expression.Pop());
+
+while (expression.Count > 0)
+{
+    string sign = expression.Pop();
+    int number = int.Parse(expression.Pop());
+
+    if (sign == "+")
+    {
+        result = result + number;
+    }
+    else if (sign == "-")
+    {
+        result = result - number;
+    }
+}
+
+Console.WriteLine(result);
