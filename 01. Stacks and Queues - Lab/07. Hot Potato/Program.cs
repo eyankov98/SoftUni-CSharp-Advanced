a87af13b391316e85bf4b2e3ddb2e5aacb2d3479
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-12 17:29:04 +0300

    Add files via upload

---
+++ 01. Stacks and Queues - Lab/07. Hot Potato/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] inputNames = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int n = int.Parse(Console.ReadLine());
+
+Queue<string> people = new Queue<string>(inputNames);
+
+while (people.Count > 1)
+{
+    for (int i = 1; i < n; i++)
+    {
+        string name = people.Dequeue();
+
+        people.Enqueue(name);
+    }
+
+    Console.WriteLine($"Removed {people.Dequeue()}");
+}
+
+Console.WriteLine($"Last is {people.Peek()}");
