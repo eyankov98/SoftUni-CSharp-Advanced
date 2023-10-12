795b773b5e134bf1c61495bff96b3dca83cf328b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-12 17:29:35 +0300

    Add files via upload

---
+++ 01. Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,32 @@
+﻿Queue<string> trafficJam = new Queue<string>();
+
+int numberOfCars = int.Parse(Console.ReadLine());
+
+string command = Console.ReadLine();
+
+int carCount = 0;
+
+while (command != "end")
+{
+    if (command != "green")
+    {
+        trafficJam.Enqueue(command);
+        command = Console.ReadLine();
+        continue;
+    }
+
+    int currentCount = numberOfCars;
+
+    while (trafficJam.Any() && currentCount > 0)
+    {
+        Console.WriteLine($"{trafficJam.Dequeue()} passed!");
+
+        currentCount--;
+
+        carCount++;
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{carCount} cars passed the crossroads.");
