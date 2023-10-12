7fcdf478736c0669aef6fa0420c35d316e7f3a86
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-12 17:28:00 +0300

    Add files via upload

---
+++ 01. Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> numbers = new Queue<int>(inputNumbers);
+
+string result = string.Empty;
+
+while (numbers.Any())
+{
+    int number = numbers.Dequeue();
+
+    if (number % 2 == 0)
+    {
+        result = result + $"{number}, ";
+    }
+}
+
+Console.WriteLine(result.TrimEnd(' ', ','));
