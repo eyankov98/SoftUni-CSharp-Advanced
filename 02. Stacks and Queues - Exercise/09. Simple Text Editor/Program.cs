5175c8d752dd9454d69730783b7833a79b4d539f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:25:58 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,42 @@
+﻿string text = string.Empty;
+
+int countOfOperations = int.Parse(Console.ReadLine());
+
+Stack<string> stack = new Stack<string>();
+
+for (int i = 0; i < countOfOperations; i++)
+{
+    string[] command = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    int currentCommand = int.Parse(command[0]);
+
+    if (currentCommand == 1)
+    {
+        stack.Push(text);
+
+        string tokens = command[1];
+
+        text = text + tokens;
+    }
+    else if (currentCommand == 2)
+    {
+        stack.Push(text);
+
+        int count = int.Parse(command[1]);
+
+        text = text.Remove(text.Length - count);
+    }
+    else if (currentCommand == 3)
+    {
+        int index = int.Parse(command[1]);
+
+        index = index - 1;
+
+        Console.WriteLine(text[index]);
+    }
+    else if (currentCommand == 4)
+    {
+        text = stack.Pop();
+    }
+}
