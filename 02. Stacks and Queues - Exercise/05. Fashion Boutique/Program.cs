619eef69753cc49bff7efa664b8928e210778893
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:23:39 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> countClothes = new Stack<int>(inputNumbers);
+
+int capacityOfRack = int.Parse(Console.ReadLine());
+
+int currentCapacityOfRack = capacityOfRack;
+
+int countRacks = 1;
+
+while (countClothes.Any())
+{
+    currentCapacityOfRack = currentCapacityOfRack - countClothes.Peek();
+
+    if (currentCapacityOfRack < 0)
+    {
+        currentCapacityOfRack = capacityOfRack;
+
+        countRacks++;
+
+        continue;
+    }
+
+    countClothes.Pop();
+}
+
+Console.WriteLine(countRacks);
