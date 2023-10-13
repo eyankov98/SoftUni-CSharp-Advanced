b2d8ae83067744dac8ce1a7c7ee74055129d617b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:24:50 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -0,0 +1,52 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Queue<int[]> pumps = new Queue<int[]>();
+
+int litersPerKilometer = 1;
+
+for (int i = 0; i < n; i++)
+{
+    int[] inputNumber = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    pumps.Enqueue(inputNumber);
+}
+
+int startPetrolPump = 0;
+
+while (true)
+{
+    bool isComplete = true;
+    int totalLiters = 0;
+
+    foreach (var number in pumps)
+    {
+        int liters = number[0];
+        int kilometers = number[1];
+
+        totalLiters = totalLiters + liters;
+
+        totalLiters = totalLiters - kilometers * litersPerKilometer;
+
+        if (totalLiters < 0)
+        {
+            int[] currentPumps = pumps.Dequeue();
+
+            pumps.Enqueue(currentPumps);
+
+            startPetrolPump++;
+
+            isComplete = false;
+
+            break;
+        }
+    }
+
+    if (isComplete)
+    {
+        Console.WriteLine(startPetrolPump);
+        break;
+    }
+}
