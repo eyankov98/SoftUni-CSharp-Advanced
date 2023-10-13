b806968039f142a60f1466697aaa5a06b083e7af
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:26:27 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+int greenLightDuration = int.Parse(Console.ReadLine());
+int windowDuration = int.Parse(Console.ReadLine());
+
+Queue<StringBuilder> cars = new Queue<StringBuilder>();
+int carsPassedCounter = 0;
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "END")
+{
+    if (command == "green")
+    {
+        int remainingSeconds = greenLightDuration;
+
+        while (remainingSeconds > 0 && cars.Count > 0)
+        {
+            StringBuilder currentCar = cars.Dequeue();
+            string currentCarString = currentCar.ToString();
+
+            if (currentCar.Length <= remainingSeconds)
+            {
+                remainingSeconds -= currentCar.Length;
+                carsPassedCounter++;
+            }
+            else
+            {
+                currentCar.Remove(0, remainingSeconds);
+                remainingSeconds = 0;
+
+                if (currentCar.Length <= windowDuration)
+                {
+                    carsPassedCounter++;
+                }
+                else
+                {
+                    char hitAtChar = currentCar[windowDuration];
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{currentCarString} was hit at {hitAtChar}.");
+                    return;
+                }
+            }
+        }
+    }
+    else
+    {
+        StringBuilder currentCar = new StringBuilder(command);
+        cars.Enqueue(currentCar);
+    }
+}
+
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{carsPassedCounter} total cars passed the crossroads.");
