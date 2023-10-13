123452ea0d30371dddab5f0e03f979d5186f20ba
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:22:46 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,39 @@
+﻿Stack<int> numbers = new Stack<int>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int[] inputNumbers = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    int command = inputNumbers[0];
+
+    if (command == 1)
+    {
+        int number = inputNumbers[1];
+        numbers.Push(number);
+    }
+    else if (command == 2)
+    {
+        numbers.Pop();
+    }
+    else if (command == 3)
+    {
+        if (numbers.Any())
+        {
+            Console.WriteLine(numbers.Max());
+        }
+    }
+    else if (command == 4)
+    {
+        if (numbers.Any())
+        {
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", numbers));
