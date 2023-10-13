4a4cfc1f975468f9a21b9ae16a6d0e317b5a0af0
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:24:09 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] inputSongs = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+Queue<string> songsQueue = new Queue<string>(inputSongs);
+
+while (songsQueue.Any())
+{
+    string[] commandInfo = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string currentCommand = commandInfo[0];
+
+    if (currentCommand == "Play")
+    {
+        songsQueue.Dequeue();
+    }
+    else if (currentCommand == "Add")
+    {
+        //string song = string.Join(" ", commandInfo.Skip(1));
+        string song = string.Join(" ", commandInfo[1..]);
+
+        if (songsQueue.Contains(song))
+        {
+            Console.WriteLine($"{song} is already contained!");
+        }
+        else
+        {
+            songsQueue.Enqueue(song);
+        }
+    }
+    else if (currentCommand == "Show")
+    {
+        Console.WriteLine(string.Join(", ", songsQueue));
+    }
+}
+
+Console.WriteLine("No more songs!");
