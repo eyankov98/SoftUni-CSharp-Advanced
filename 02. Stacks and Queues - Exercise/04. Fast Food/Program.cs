2e1f02f7b99b19a028906881c96fda646b133fa4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:23:14 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,31 @@
+﻿int quantityOfFood = int.Parse(Console.ReadLine());
+
+int[] quantityOfOrders = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> orders = new Queue<int>(quantityOfOrders);
+
+Console.WriteLine(orders.Max());
+
+while (orders.Any())
+{
+    quantityOfFood = quantityOfFood - orders.Peek();
+
+    if (quantityOfFood < 0)
+    {
+        break;
+    }
+
+    orders.Dequeue();
+}
+
+if (orders.Any())
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+}
+else
+{
+    Console.WriteLine("Orders complete");
+}
