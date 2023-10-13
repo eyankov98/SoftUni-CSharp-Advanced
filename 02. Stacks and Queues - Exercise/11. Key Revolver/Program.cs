b19d7d1e3978daf13c50682cccfd84cd8a151dbc
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:27:03 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,60 @@
+﻿int bulletPrice = int.Parse(Console.ReadLine());
+
+int barrelSize = int.Parse(Console.ReadLine());
+
+int[] bullets = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] locks = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> stackOfBullets = new Stack<int>(bullets);
+
+Queue<int> queueOfLocks = new Queue<int>(locks);
+
+int valueOfIntelligence = int.Parse(Console.ReadLine());
+
+int initialBulletsCount = stackOfBullets.Count;
+
+int currentBarrelSize = barrelSize;
+
+for (int i = 0; i < initialBulletsCount; i++)
+{
+    if (stackOfBullets.Pop() <= queueOfLocks.Peek())
+    {
+        Console.WriteLine("Bang!");
+
+        queueOfLocks.Dequeue();
+    }
+    else
+    {
+        Console.WriteLine("Ping!");
+    }
+
+    currentBarrelSize--;
+
+    if (currentBarrelSize == 0 && stackOfBullets.Any())
+    {
+        Console.WriteLine("Reloading!");
+
+        currentBarrelSize = barrelSize;
+    }
+
+    if (!stackOfBullets.Any() && queueOfLocks.Any())
+    {
+        Console.WriteLine($"Couldn't get through. Locks left: {queueOfLocks.Count}");
+
+        return;
+    }
+
+    if (!queueOfLocks.Any())
+    {
+        Console.WriteLine($"{stackOfBullets.Count} bullets left. Earned ${valueOfIntelligence - (initialBulletsCount - stackOfBullets.Count) * bulletPrice}");
+
+        return;
+    }
+}
