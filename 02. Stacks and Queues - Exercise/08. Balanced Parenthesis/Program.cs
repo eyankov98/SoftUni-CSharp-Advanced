18b71bbdbec439b12854fa564b25c0fca760f5ca
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:25:32 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,61 @@
+﻿string input = Console.ReadLine();
+
+Stack<char> parentheses = new Stack<char>();
+
+foreach (var charr in input)
+{
+    if (charr == '{')
+    {
+        parentheses.Push(charr);
+    }
+    else if (charr == '[')
+    {
+        parentheses.Push(charr);
+    }
+    else if (charr == '(')
+    {
+        parentheses.Push(charr);
+    }
+
+    else if (charr == '}')
+    {
+        if (!parentheses.Any() || parentheses.Pop() != '{')
+        {
+            Console.WriteLine("NO");
+
+            return;
+        }
+
+        continue;
+    }
+    else if (charr == ']')
+    {
+        if (!parentheses.Any() || parentheses.Pop() != '[')
+        {
+            Console.WriteLine("NO");
+
+            return;
+        }
+
+        continue;
+    }
+    else if (charr == ')')
+    {
+        if (!parentheses.Any() || parentheses.Pop() != '(')
+        {
+            Console.WriteLine("NO");
+
+            return;
+        }
+
+        continue;
+    }
+}
+if (parentheses.Any())
+{
+    Console.WriteLine("NO");
+}
+else
+{
+    Console.WriteLine("YES");
+}
