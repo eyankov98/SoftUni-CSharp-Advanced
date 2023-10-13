53e18d6ccca107b3ac6679d27ae7b6ad46ec82c4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-13 19:27:44 +0300

    Add files via upload

---
+++ 02. Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] cupsCapacities = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] waterBottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> cupsQueue = new Queue<int>(cupsCapacities);
+Stack<int> waterBottlesStack = new Stack<int>(waterBottles);
+
+int wastedWater = 0;
+while (waterBottlesStack.Any() && cupsQueue.Any())
+{
+    int currentCup = cupsQueue.Dequeue();
+    int currentBottle = waterBottlesStack.Pop();
+
+    currentCup -= currentBottle;
+
+    if (currentCup <= 0)
+    {
+        wastedWater += Math.Abs(currentCup);
+        continue;
+    }
+
+    while (currentCup > 0 && waterBottles.Any())
+    {
+        currentBottle = waterBottlesStack.Pop();
+        currentCup -= currentBottle;
+
+        if (currentCup <= 0)
+        {
+            wastedWater += Math.Abs(currentCup);
+            break;
+        }
+    }
+}
+
+if (cupsQueue.Count == 0)
+{
+    Console.Write("Bottles: ");
+    Console.WriteLine(String.Join(" ", waterBottlesStack));
+}
+else
+{
+    Console.Write("Cups: ");
+    Console.WriteLine(String.Join(" ", cupsQueue));
+}
+
+Console.WriteLine($"Wasted litters of water: {wastedWater}");
