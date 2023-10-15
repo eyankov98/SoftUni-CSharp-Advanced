0065654c693c35df55c283fec8fec7ee1a4d1472
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-15 14:06:14 +0300

    Add files via upload

---
+++ 03. Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,25 @@
+﻿int matrixSize = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[matrixSize, matrixSize];
+
+for (int row = 0; row < matrixSize; row++)
+{
+    int[] matrixElements = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int colum = 0; colum < matrixSize; colum++)
+    {
+        matrix[row, colum] = matrixElements[colum];
+    }
+}
+
+int sumOfMatrixElementsOnDiagonal = 0;
+
+for (int i = 0; i < matrixSize; i++)
+{
+    sumOfMatrixElementsOnDiagonal = sumOfMatrixElementsOnDiagonal + matrix[i, i];
+}
+
+Console.WriteLine(sumOfMatrixElementsOnDiagonal);
