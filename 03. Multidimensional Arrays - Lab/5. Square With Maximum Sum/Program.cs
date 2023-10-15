4b4f45aa72825624896547c74e57e2f35991172f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-15 14:07:29 +0300

    Add files via upload

---
+++ 03. Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -0,0 +1,49 @@
+﻿string[] matrixSize = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+int rows = int.Parse(matrixSize[0]);
+int colums = int.Parse(matrixSize[1]);
+
+int[,] matrix = new int[rows, colums];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] matrixElements = Console.ReadLine()
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int colum = 0; colum < colums; colum++)
+    {
+        matrix[row, colum] = matrixElements[colum];
+    }
+}
+
+int maxSum = int.MinValue;
+int maxRow = 0;
+int maxColum = 0;
+
+for (int row = 0; row < rows - 1; row++)
+{
+    for (int colum = 0; colum < colums - 1; colum++)
+    {
+        int currentSum = 0;
+
+        currentSum = currentSum + matrix[row, colum];
+        currentSum = currentSum + matrix[row, colum + 1];
+        currentSum = currentSum + matrix[row + 1, colum];
+        currentSum = currentSum + matrix[row + 1, colum + 1];
+
+        if (currentSum > maxSum)
+        {
+            maxRow = row;
+            maxColum = colum;
+            maxSum = currentSum;
+        }
+    }
+}
+
+Console.WriteLine($"{matrix[maxRow, maxColum]} {matrix[maxRow, maxColum + 1]}");
+Console.WriteLine($"{matrix[maxRow + 1, maxColum]} {matrix[maxRow + 1, maxColum + 1]}");
+
+Console.WriteLine(maxSum);
