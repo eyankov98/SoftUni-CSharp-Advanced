f2409da1555d62c74f55381ce2f04eea42f34f57
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-15 14:06:47 +0300

    Add files via upload

---
+++ 03. Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,31 @@
+﻿int matrixSize = int.Parse(Console.ReadLine());
+
+char[,] matrix = new char[matrixSize, matrixSize];
+
+for (int row = 0; row < matrixSize; row++)
+{
+    char[] matrixElements = Console.ReadLine()
+        .ToCharArray();
+
+    for (int colum = 0; colum < matrixSize; colum++)
+    {
+        matrix[row, colum] = matrixElements[colum];
+    }
+}
+
+char symbol = char.Parse(Console.ReadLine());
+
+for (int row = 0; row < matrixSize; row++)
+{
+    for (int colum = 0; colum < matrixSize; colum++)
+    {
+        if (matrix[row, colum] == symbol)
+        {
+            Console.WriteLine($"({row}, {colum})");
+
+            return;
+        }
+    }
+}
+
+Console.WriteLine($"{symbol} does not occur in the matrix");
