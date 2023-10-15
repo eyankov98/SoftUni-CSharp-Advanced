351c7955f27f68c283bfd6f0ab087e7534d13a82
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-15 14:05:14 +0300

    Add files via upload

---
+++ 03. Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] matrixSize = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+int rows = int.Parse(matrixSize[0]);
+int colums = int.Parse(matrixSize[1]);
+
+int[,] matrix = new int[rows, colums];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] matrixElements = Console.ReadLine()
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int colum = 0; colum < colums; colum++)
+    {
+        matrix[row, colum] = matrixElements[colum];
+    }
+}
+
+int sumMatrixElements = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    for (int colum = 0; colum < colums; colum++)
+    {
+        sumMatrixElements = sumMatrixElements + matrix[row, colum];
+    }
+}
+
+Console.WriteLine(rows);
+Console.WriteLine(colums);
+Console.WriteLine(sumMatrixElements);
