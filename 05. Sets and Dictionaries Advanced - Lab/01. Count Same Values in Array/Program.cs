b18556d66edfdadae78683b64235bd6d7db040c0
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-17 14:51:04 +0300

    Add files via upload

---
+++ 05. Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,21 @@
+﻿List<double> list = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToList();
+
+Dictionary<double, int> numberOfOccurrences = new Dictionary<double, int>();
+
+foreach (var number in list)
+{
+    if (!numberOfOccurrences.ContainsKey(number))
+    {
+        numberOfOccurrences.Add(number, 0);
+    }
+
+    numberOfOccurrences[number]++;
+}
+
+foreach (var number in numberOfOccurrences)
+{
+    Console.WriteLine($"{number.Key} - {number.Value} times");
+}
