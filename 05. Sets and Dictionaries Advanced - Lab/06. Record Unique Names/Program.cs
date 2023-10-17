5f5152bb36b9f2aeb3762eb6022e9e329965f119
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-17 14:54:01 +0300

    Add files via upload

---
+++ 05. Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,13 @@
+﻿HashSet<string> names = new HashSet<string>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    names.Add(Console.ReadLine());
+}
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
