bcc5b95c3799110750104a79d62a81fc9a7f28ba
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-17 14:54:39 +0300

    Add files via upload

---
+++ 05. Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,35 @@
+﻿HashSet<string> carsNumbers = new HashSet<string>();
+
+string command = Console.ReadLine();
+
+while (command != "END")
+{
+    string[] input = command
+        .Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+    string currendCommand = input[0];
+    string carNumber = input[1];
+
+    if (currendCommand == "IN")
+    {
+        carsNumbers.Add(carNumber);
+    }
+    else if (currendCommand == "OUT")
+    {
+        carsNumbers.Remove(carNumber);
+    }
+
+    command = Console.ReadLine();
+}
+
+if (carsNumbers.Any())
+{
+    foreach (var carNumber in carsNumbers)
+    {
+        Console.WriteLine(carNumber);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
