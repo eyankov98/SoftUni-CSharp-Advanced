b0ebe531af7c5246757204f40d3c13690f0cdbbf
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-17 14:55:14 +0300

    Add files via upload

---
+++ 05. Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,54 @@
+﻿HashSet<string> vipGuests = new HashSet<string>();
+HashSet<string> regularGuests = new HashSet<string>();
+
+string command = Console.ReadLine();
+
+while (command != "PARTY")
+{
+    string currentGuest = command;
+
+    if (Char.IsDigit(currentGuest[0]))
+    {
+        vipGuests.Add(currentGuest);
+    }
+    else
+    {
+        regularGuests.Add(currentGuest);
+    }
+
+    command = Console.ReadLine();
+}
+
+while (command != "END")
+{
+    string currentGuest = command;
+
+    if (Char.IsDigit(currentGuest[0]))
+    {
+        vipGuests.Remove(currentGuest);
+    }
+    else
+    {
+        regularGuests.Remove(currentGuest);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(vipGuests.Count + regularGuests.Count);
+
+if (vipGuests.Any())
+{
+    foreach (var vipGuest in vipGuests)
+    {
+        Console.WriteLine(vipGuest);
+    }
+}
+
+if (regularGuests.Any())
+{
+    foreach (var regularGuest in regularGuests)
+    {
+        Console.WriteLine(regularGuest);
+    }
+}
