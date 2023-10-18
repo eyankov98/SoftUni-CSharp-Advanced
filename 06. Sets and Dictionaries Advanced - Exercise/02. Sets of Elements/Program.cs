e2b002932e7215817e27d6ee7322f7fbb229d27b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-18 18:44:43 +0300

    Add files via upload

---
+++ 06. Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿HashSet<int> firstSet = new HashSet<int>();
+HashSet<int> SecondSet = new HashSet<int>();
+
+int[] countOfElements = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int n = countOfElements[0];
+int m = countOfElements[1];
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    firstSet.Add(number);
+}
+
+for (int i = 0; i < m; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    SecondSet.Add(number);
+}
+
+firstSet.IntersectWith(SecondSet);
+
+Console.WriteLine(string.Join(" ", firstSet));
