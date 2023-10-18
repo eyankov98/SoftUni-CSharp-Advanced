8dad6af610bebb1f45a22c30af6aeaf0f658460f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-18 18:46:17 +0300

    Add files via upload

---
+++ 06. Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,18 @@
+﻿SortedDictionary<char, int> symbolsCounts = new SortedDictionary<char, int>();
+
+string input = Console.ReadLine();
+
+foreach (var symbol in input)
+{
+    if (!symbolsCounts.ContainsKey(symbol))
+    {
+        symbolsCounts.Add(symbol, 0);
+    }
+
+    symbolsCounts[symbol]++;
+}
+
+foreach (var symbol in symbolsCounts)
+{
+    Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+}
