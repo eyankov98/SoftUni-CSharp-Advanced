610f73a8568f0888334b90d70f23a16cfd41c692
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-18 18:45:27 +0300

    Add files via upload

---
+++ 06. Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,13 @@
+﻿SortedSet<string> periodicElements = new SortedSet<string>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] element = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    periodicElements.UnionWith(element);
+}
+
+Console.WriteLine(string.Join(" ", periodicElements));
