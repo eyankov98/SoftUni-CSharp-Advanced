5b984a7051ee3260bc5a99347ccb6338cfe8b692
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-18 18:46:34 +0300

    Add files via upload

---
+++ 06. Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,19 @@
+﻿Dictionary<int, int> numbersCounts = new Dictionary<int, int>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int inputNumber = int.Parse(Console.ReadLine());
+
+    if (!numbersCounts.ContainsKey(inputNumber))
+    {
+        numbersCounts.Add(inputNumber, 0);
+    }
+
+    numbersCounts[inputNumber]++;
+}
+
+int number = numbersCounts.Single(nc => nc.Value % 2 == 0).Key;
+
+Console.WriteLine(number);
