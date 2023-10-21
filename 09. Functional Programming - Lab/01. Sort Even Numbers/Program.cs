128ca73ffb3e1c0f2134c3e7c2a27afb2b59f105
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-21 22:51:39 +0300

    Add files via upload

---
+++ 09. Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] integers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Where(n => n % 2 == 0)
+    .OrderBy(n => n)
+    .ToArray();
+
+Console.WriteLine(String.Join(", ", integers));
