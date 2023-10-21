bbefe14aecf0a55534e546ed715c9baf98c9e6eb
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-21 22:51:54 +0300

    Add files via upload

---
+++ 09. Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Func<string, int> parser = n => int.Parse(n);
+
+int[] numbers = Console.ReadLine()
+    .Split(", ")
+    .Select(parser)
+    .ToArray();
+
+Console.WriteLine(numbers.Length);
+Console.WriteLine(numbers.Sum());
