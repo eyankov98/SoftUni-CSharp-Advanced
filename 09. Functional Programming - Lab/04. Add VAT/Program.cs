2d45042479ef19207742260859047744da872453
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-21 22:52:29 +0300

    Add files via upload

---
+++ 09. Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,13 @@
+﻿Func<double, double> addingVat = n => n + (n * 0.2);
+
+double[] prices = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToArray();
+
+for (int i = 0; i < prices.Length; i++)
+{
+    prices[i] = addingVat(prices[i]);
+
+    Console.WriteLine($"{prices[i]:f2}");
+}
