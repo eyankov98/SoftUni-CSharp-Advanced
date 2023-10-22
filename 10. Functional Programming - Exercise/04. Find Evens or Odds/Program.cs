04c4047fdd525bdd9fd085ec50d0fba47ec05164
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-22 20:39:52 +0300

    Add files via upload

---
+++ 10. Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,33 @@
+﻿Func<string, int[]> readIntegers = input => input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(int.Parse)
+.ToArray();
+
+int[] inputNumbers = readIntegers(Console.ReadLine());
+int lowerBond = inputNumbers[0];
+int upperBond = inputNumbers[1];
+
+string command = Console.ReadLine();
+Predicate<int> numbersSelector = number =>
+{
+    if (command == "even")
+    {
+        return number % 2 == 0;
+    }
+    else if (command == "odd")
+    {
+        return number % 2 != 0;
+    }
+    return false;
+};
+
+List<int> outputNumbers = new List<int>();
+
+for (int i = lowerBond; i <= upperBond; i++)
+{
+    if (numbersSelector(i))
+    {
+        outputNumbers.Add(i);
+    }
+}
+
+Console.WriteLine(String.Join(" ", outputNumbers));
