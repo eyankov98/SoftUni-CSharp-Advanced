1c0285336e6a3cf257eef0e3c5dbaa3c1b039c93
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-22 20:40:50 +0300

    Add files via upload

---
+++ 10. Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,19 @@
+﻿int nameLength = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+Predicate<string> nameChecker = name => name.Length <= nameLength;
+
+List<string> validNames = new List<string>();
+
+for (int i = 0; i < names.Length; i++)
+{
+    if (nameChecker(names[i]))
+    {
+        validNames.Add(names[i]);
+    }
+}
+
+Console.WriteLine(string.Join("\n", validNames));
