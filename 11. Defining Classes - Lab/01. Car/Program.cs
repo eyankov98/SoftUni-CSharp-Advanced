4d64e29921752f5e8ccfbab4a007b638ec058abc
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-23 23:45:19 +0300

    Add files via upload

---
+++ 11. Defining Classes - Lab/01. Car/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car()
+            {
+                Make = "BMW",
+                Model = "X3",
+                Year = 2006
+            };
+
+            Console.WriteLine(car.Make);
+            Console.WriteLine(car.Model);
+            Console.WriteLine(car.Year);
+        }
+    }
+}
