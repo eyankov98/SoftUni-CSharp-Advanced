2b00a6a3819ae050d09ca8e23e0e4c5fadcd7fdb
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-24 21:19:19 +0300

    Add files via upload

---
+++ 12. Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person firstPerson = new Person()
+        {
+            Name = "Peter",
+            Age = 20
+        };
+
+        Person secondPerson = new Person()
+        {
+            Name = "George",
+            Age = 18
+        };
+
+        Person thirdPerson = new Person()
+        {
+            Name = "Jose",
+            Age = 43
+        };
+
+        Console.WriteLine($"Name: {firstPerson.Name}, Age: {firstPerson.Age}");
+        Console.WriteLine($"Name: {secondPerson.Name}, Age: {secondPerson.Age}");
+        Console.WriteLine($"Name: {thirdPerson.Name}, Age: {thirdPerson.Age}");
+    }
+}
