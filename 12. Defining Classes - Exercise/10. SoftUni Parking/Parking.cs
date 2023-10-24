836a4e4e409ea88f95bbce41ace272c8939665b1
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-24 22:05:10 +0300

    Add files via upload

---
+++ 12. Defining Classes - Exercise/10. SoftUni Parking/Parking.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+		private Dictionary<string, Car> cars;
+		private int capacity;
+
+		public Parking(int capacity)
+		{
+			cars = new Dictionary<string, Car>();
+
+			this.capacity = capacity;
+		}
+
+		public int Count { get { return cars.Count; } }
+
+		public string AddCar(Car car)
+		{
+			if (cars.ContainsKey(car.RegistrationNumber))
+			{
+				return "Car with that registration number, already exists!";
+            }
+
+            if (this.Count >= this.capacity)
+            {
+                return "Parking is full!";
+            }
+
+			this.cars.Add(car.RegistrationNumber, car);
+
+			return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+		public Car GetCar(string registrationNumber)
+		{
+			return cars[registrationNumber];
+		}
+
+		public string RemoveCar (string registrationNumber)
+		{
+			if (!cars.ContainsKey(registrationNumber))
+			{
+				return "Car with that registration number, doesn't exist!";
+
+            }
+
+			cars.Remove(registrationNumber);
+
+			return $"Successfully removed {registrationNumber}";
+        }
+
+		public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+		{
+			foreach (string registrationNumber in registrationNumbers)
+			{
+                this.RemoveCar(registrationNumber);
+			}
+		}
+    }
+}
