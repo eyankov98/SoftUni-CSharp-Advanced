f532c88aba5769e46701f4c5da4e49d5425a0014
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-24 21:20:21 +0300

    Add files via upload

---
+++ 12. Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person firstPerson = new Person("Peter", 20);
+
+        Person secondPerson = new Person("George", 18); 
+
+        Person thirdPerson = new Person("Jose", 43);
+
+        Console.WriteLine($"Name: {firstPerson.Name}, Age: {firstPerson.Age}");
+        Console.WriteLine($"Name: {secondPerson.Name}, Age: {secondPerson.Age}");
+        Console.WriteLine($"Name: {thirdPerson.Name}, Age: {thirdPerson.Age}");
+    }
+}
