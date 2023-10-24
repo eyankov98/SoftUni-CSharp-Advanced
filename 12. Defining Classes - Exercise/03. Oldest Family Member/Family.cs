3730bc486af2448e95c71f9fa643eb7fde6e2f3d
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-24 21:21:14 +0300

    Add files via upload

---
+++ 12. Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            people = new List<Person>();
+        }
+
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = value; }
+        }
+
+
+        public void AddMember(Person person)
+        {
+            people.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return people.MaxBy(p => p.Age);
+        }
+    }
+}
---
+++ 12. Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Family family = new Family();
+
+        int countOfPeople = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i  < countOfPeople; i ++)
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = personInfo[0];
+            int age = int.Parse(personInfo[1]);
+
+            Person person = new Person(name, age);
+
+            family.AddMember(person);
+        }
+
+        Person oldestPerson = family.GetOldestMember();
+
+        Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+    }
+}
