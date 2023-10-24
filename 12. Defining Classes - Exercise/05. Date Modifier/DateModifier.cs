561c1b1d787fa44f90e31b81990d97300f68288b
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-24 21:23:20 +0300

    Add files via upload

---
+++ 12. Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+		public static int GetDifferenceInDays(string firstDate, string secondDate)
+		{
+			DateTime startDate = DateTime.Parse(firstDate);
+			DateTime endDate = DateTime.Parse(secondDate);
+
+			TimeSpan difference = endDate - startDate;
+
+			return Math.Abs(difference.Days);
+		}
+	}
+}
---
+++ 12. Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace DateModifier;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        string firstDate = Console.ReadLine();
+        string secondDate = Console.ReadLine();
+
+        int differenceInDays = DateModifier.GetDifferenceInDays(firstDate, secondDate);
+
+        Console.WriteLine(differenceInDays);
+    }
+}
