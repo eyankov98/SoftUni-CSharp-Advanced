795ae8407a7751cd1383e10948237b2a7093b79a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-24 21:23:40 +0300

    Add files via upload

---
+++ 12. Defining Classes - Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+		private string model;
+		private double fuelAmount;
+		private double fuelConsumptionPerKilometer;
+		private double travelledDistance;
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		public double FuelAmount
+		{
+			get { return fuelAmount; }
+			set { fuelAmount = value; }
+		}
+
+		public double FuelConsumptionPerKilometer
+		{
+			get { return fuelConsumptionPerKilometer; }
+			set { fuelConsumptionPerKilometer = value; }
+		}
+
+		public double TravelledDistance
+		{
+			get { return travelledDistance; }
+			set { travelledDistance = value; }
+		}
+
+		public void DriveCar(double amountOfKm)
+		{
+			double litersNeeded = amountOfKm * fuelConsumptionPerKilometer;
+
+			if (litersNeeded > fuelAmount)
+			{
+				Console.WriteLine("Insufficient fuel for the drive");
+			}
+			else
+			{
+				fuelAmount -= litersNeeded;
+				travelledDistance += amountOfKm;
+			}
+		}
+	}
+}
---
+++ 12. Defining Classes - Exercise/06. Speed Racing/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace SpeedRacing;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Car> cars = new List<Car>();
+
+        int countOfCars = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countOfCars; i++)
+        {
+            string[] carsInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string model = carsInfo[0];
+            double fuelAmount = double.Parse(carsInfo[1]);
+            double fuelConsumptionFor1km = double.Parse(carsInfo[2]);
+
+            Car car = new Car()
+            {
+                Model = model,
+                FuelAmount = fuelAmount,
+                FuelConsumptionPerKilometer = fuelConsumptionFor1km,
+                TravelledDistance = 0
+            };
+
+            cars.Add(car);
+        }
+
+        string command = Console.ReadLine();
+
+        while (command != "End")
+        {
+            string[] commandInfo = command
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            
+            string carModel = commandInfo[1];
+            double amountOfKm = double.Parse(commandInfo[2]);
+
+            Car car = cars.FirstOrDefault(c => c.Model == carModel);
+
+            car.DriveCar(amountOfKm);
+
+            command= Console.ReadLine();
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+        }
+    }
+}
