c8dc23d83db3043628b3bc80297cd46b34881fe2
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-24 21:22:46 +0300

    Add files via upload

---
+++ 12. Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Person> persons = new List<Person>();
+
+        int countOfPeople = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countOfPeople; i++)
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = personInfo[0];
+            int age = int.Parse(personInfo[1]);
+
+            Person person = new Person(name, age);
+
+            persons.Add(person);
+        }
+
+        foreach (var person in persons.OrderBy(p => p.Name))
+        {
+            if (person.Age > 30)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
