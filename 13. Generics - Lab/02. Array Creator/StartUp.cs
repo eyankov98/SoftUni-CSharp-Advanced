6a586cd458a35df58b32178f68bc53520685ed3a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-25 20:27:59 +0300

    Add files via upload

---
+++ 13. Generics - Lab/02. Array Creator/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericArrayCreator;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        float[] numbers = ArrayCreator.Create(50, 3f);
+        string[] strings = ArrayCreator.Create(3, "Edgar");
+        int[] integers = ArrayCreator.Create(10, 33);
+
+        Console.WriteLine(string.Join(", ", numbers));
+        Console.WriteLine(string.Join(", ", strings));
+        Console.WriteLine(string.Join(", ", integers));
+    }
+}
