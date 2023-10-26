edac9a71a4fbe7bf8356551627ab2e195b6b491a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-26 15:35:00 +0300

    Add files via upload

---
+++ 14. Generics - Exercise/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace BoxOfString;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Box<string> box = new Box<string>();
+
+        int countOfNumbers = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countOfNumbers; i++)
+        {
+            string item = Console.ReadLine();
+
+            box.Add(item);
+        }
+
+        Console.WriteLine(box.ToString());
+    }
+}
