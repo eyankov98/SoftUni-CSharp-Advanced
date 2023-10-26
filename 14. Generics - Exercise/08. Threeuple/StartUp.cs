a1ca1965eb3b4515cc30d849f221280e8fdc7a75
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-26 15:37:41 +0300

    Add files via upload

---
+++ 14. Generics - Exercise/08. Threeuple/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace Threeuple;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        string[] personInfo = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string firstName = personInfo[0];
+        string lastName = personInfo[1];
+        string address = personInfo[2];
+        string town = string.Join(" ", personInfo[3..]); // '..' - (to the end) 
+
+        string[] drinkingBeerInfo = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string name = drinkingBeerInfo[0];
+        int litersOfBeer = int.Parse(drinkingBeerInfo[1]);
+        bool drunkOrNot = drinkingBeerInfo[2] == "drunk";
+
+        string[] bankInfo = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string accountName = bankInfo[0];
+        double accountBalance = double.Parse(bankInfo[1]);
+        string bankName = bankInfo[2];
+
+        CustomeThreeuple<string, string, string> nameAddressTown = new CustomeThreeuple<string, string, string>($"{firstName} {lastName}", address, town);
+        CustomeThreeuple<string, int, bool> nameLitersOfBeerDrunkOrNot = new CustomeThreeuple<string, int, bool>(name, litersOfBeer, drunkOrNot);
+        CustomeThreeuple<string, double, string> nameAccountBankBankName = new CustomeThreeuple<string, double, string>(accountName, accountBalance, bankName);
+
+        Console.WriteLine(nameAddressTown.ToString());
+        Console.WriteLine(nameLitersOfBeerDrunkOrNot.ToString());
+        Console.WriteLine(nameAccountBankBankName.ToString());
+    }
+}
