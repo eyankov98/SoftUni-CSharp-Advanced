7fabfc23684f206108f338c6e05c3a26ef5adc88
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-26 15:35:19 +0300

    Add files via upload

---
+++ 14. Generics - Exercise/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfInteger
+{
+    public class Box<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 14. Generics - Exercise/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace BoxOfInteger;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Box<int> box = new Box<int>();
+
+        int countOfNumbers = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countOfNumbers; i++)
+        {
+            int item = int.Parse(Console.ReadLine());
+
+            box.Add(item);
+        }
+
+        Console.WriteLine(box.ToString());
+    }
+}
