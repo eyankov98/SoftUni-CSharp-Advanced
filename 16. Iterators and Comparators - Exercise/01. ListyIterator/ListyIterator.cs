28bb2fa0b14ef014aebadc8d0d30d18620514f65
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-30 21:04:40 +0200

    Add files via upload

---
+++ 16. Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,40 @@
+﻿namespace ListyIterator;
+
+public class ListyIterator<T>
+{
+    public ListyIterator(List<T> items)
+    {
+        Items = items;
+    }
+
+    private int Index;
+    private List<T> Items { get; set; }
+
+    public bool Move()
+    {
+        if (Index < Items.Count-1)
+        {
+            Index++;
+            return true;
+        }
+        
+        return false;
+    }
+
+    public bool HasNext()
+    {
+        return Index < Items.Count-1;
+    }
+
+    public void Print()
+    {
+        if (Items.Count > 0)
+        {
+            Console.WriteLine(Items[Index]);
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid Operation!");
+        }
+    }
+}
