4ab977b969e24cae28c84a02c228e15b6641c285
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-10-31 22:11:31 +0200

    Add files via upload

---
+++ 17. CSharp Advanced Exam/01. Rubber Duck Debugers/Program.cs
@@ -0,0 +1,59 @@
+﻿int[] programmerTime = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] numberOfTasks = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> times = new Queue<int>(programmerTime);
+Stack<int> tasks = new Stack<int>(numberOfTasks);
+
+Dictionary<string, int> rubberDucks = new Dictionary<string, int>()
+{
+    { "Darth Vader Ducky", 0 },
+    { "Thor Ducky", 0 },
+    { "Big Blue Rubber Ducky", 0 },
+    { "Small Yellow Rubber Ducky", 0 }
+};
+
+while (times.Any() && tasks.Any())
+{
+    int currentTime = times.Dequeue();
+    int currentTask = tasks.Pop();
+
+    int result = currentTime * currentTask;
+
+    if (result >= 0 && result <= 60)
+    {
+        rubberDucks["Darth Vader Ducky"]++;
+    }
+    else if (result >= 61 && result <= 120)
+    {
+        rubberDucks["Thor Ducky"]++;
+    }
+    else if (result >= 121 && result <= 180)
+    {
+        rubberDucks["Big Blue Rubber Ducky"]++;
+    }
+    else if (result >= 181 && result <= 240)
+    {
+        rubberDucks["Small Yellow Rubber Ducky"]++;
+    }
+    else if (result > 240)
+    {
+        currentTask -= 2;
+
+        tasks.Push(currentTask);
+        times.Enqueue(currentTime);
+    }
+}
+
+Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+
+foreach (var rubberDuck in rubberDucks)
+{
+    Console.WriteLine($"{rubberDuck.Key}: {rubberDuck.Value}");
+}
