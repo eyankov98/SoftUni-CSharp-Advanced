ab1bee63af3c66563c378e7b9f384714baa38f84
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-01 23:08:54 +0200

    Add files via upload

---
+++ 18. CSharp Advanced Exam/03. SoftUni Kindergarten/Kindergarten.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new List<Child>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Child> Registry { get; set; }
+
+        public bool AddChild(Child child)
+        {
+            if (Registry.Count < Capacity)
+            {
+                Registry.Add(child);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveChild(string childFullName) =>
+            Registry.Remove(Registry.FirstOrDefault(ch => ch.FirstName == childFullName.Split(" ")[0] && ch.LastName == childFullName.Split(" ")[1]));
+
+        public int ChildrenCount => Registry.Count;
+
+        public Child GetChild(string childFullName) =>
+            Registry.FirstOrDefault(ch => ch.FirstName == childFullName.Split(" ")[0] && ch.LastName == childFullName.Split(" ")[1]);
+
+        public string RegistryReport()
+        {
+            var sortedChildren = Registry.OrderByDescending(ch => ch.Age).ThenBy(ch => ch.LastName).ThenBy(ch => ch.FirstName).ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Registered children in {Name}:");
+
+            foreach (var child in sortedChildren)
+            {
+                sb.AppendLine(child.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
