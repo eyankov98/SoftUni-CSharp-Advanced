b5011d9cebca7f7bc6b00439278f055a9213fa43
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-02 20:36:15 +0200

    Add files via upload

---
+++ 19. CSharp Advanced Exam/03. Shoe Store/ShoeStore.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoeStore
+{
+    public class ShoeStore
+    {
+        public ShoeStore(string name, int storageCapacity)
+        {
+            Name = name;
+            StorageCapacity = storageCapacity;
+            Shoes = new List<Shoe>();
+        }
+
+        public string Name { get; set; }
+        public int StorageCapacity { get; set; }
+        public List<Shoe> Shoes { get; set; }
+
+        public int Count 
+            => Shoes.Count;
+
+        public string AddShoe(Shoe shoe)
+        {
+            if (StorageCapacity > Shoes.Count)
+            {
+                Shoes.Add(shoe);
+
+                return $"Successfully added {shoe.Type} {shoe.Material} pair of shoes to the store.";
+            }
+
+            return "No more space in the storage room.";
+        }
+
+        public int RemoveShoes(string material)
+        {
+            int removeShoes = 0;
+
+            for (int i = 0; i < Shoes.Count; i++)
+            {
+                if (Shoes[i].Material == material.ToLower())
+                {
+                    Shoes.RemoveAt(i--);
+
+                    removeShoes++;
+                }
+            }
+
+            return removeShoes;
+        }
+
+        public List<Shoe> GetShoesByType(string type)
+        {
+            List<Shoe> listToReturn = new List<Shoe>();
+
+            foreach (var shoe in Shoes)
+            {
+                if (shoe.Type == type.ToLower())
+                {
+                    listToReturn.Add(shoe);
+                }
+            }
+
+            return listToReturn;
+        }
+
+        public Shoe GetShoeBySize(double size)
+            => Shoes.FirstOrDefault(sh => sh.Size == size);
+
+        public string StockList(double size, string type)
+        {
+            List<Shoe> stockList =Shoes.Where(sh => sh.Size == size && sh.Type == type).ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            if (stockList.Count == 0)
+            {
+                sb.AppendLine("No matches found!");
+            }
+            else
+            {
+                sb.AppendLine($"Stock list for size {size} - {type} shoes:");
+
+                foreach (var shoe1 in stockList)
+                {
+                    sb.AppendLine(shoe1.ToString());
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
